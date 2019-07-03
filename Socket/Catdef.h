60ae613f5d6ac623db6baafb75b072184aa9aded
destiny <zzp>
2019-07-03 17:47:43 +0800

    linux test

--- Socket/Catdef.h
+++ Socket/Catdef.h
@@ -15,9 +15,22 @@
 #include <WinSock2.h>
 
 #else
+#include <time.h>
+#include <sys/errno.h>
+#include <fcntl.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/epoll.h>
+#include <sys/select.h>
 #endif
+
+#define _OUT(format, ...) printf((format), ##__VA_ARGS__)
+
+#define ERROR(format, ...) \
+do { \
+	_OUT("[ERROR]%s %s(Line %d): ", __FILE__, __FUNCTION__, __LINE__); \
+	_OUT((format), ##__VA_ARGS__); \
+} while(0)
 
 #endif
 
