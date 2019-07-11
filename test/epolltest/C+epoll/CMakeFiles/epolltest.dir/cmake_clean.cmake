e681a36a5ee7f89c14e3e05b93de5530757941c9
destiny <zzp>
2019-07-11 14:27:16 +0800

    new C++epoll

---
+++ test/epolltest/C+epoll/CMakeFiles/epolltest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/epolltest.dir/epolltest.cpp.o"
+  "build/Bin/epolltest.pdb"
+  "build/Bin/epolltest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/epolltest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
