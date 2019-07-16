8289151710a113d1b167b9068d0796eb8083a966
win10-31 <win10-31>
2019-07-16 18:09:26 +0800

    new api

---
+++ SrcLib/CatIocp/Cat_Iocp.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../../Network/C+Catnet/Cat_net.h"
+
+namespace IOCP
+{
+    class CatIocp : public CATNET::CatNet
+    {
+        public:
+            CatIocp();
+            ~CatIocp();
+
+            
+        private:
+        
+    };
+}
