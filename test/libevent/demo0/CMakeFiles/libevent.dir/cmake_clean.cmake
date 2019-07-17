947e74609cbcdb07af87b5cc20582d571398c6a4
destiny <zzp>
2019-07-17 20:11:43 +0800

    new libevent

---
+++ test/libevent/demo0/CMakeFiles/libevent.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libevent.dir/test.c.o"
+  "libevent.pdb"
+  "libevent"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libevent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
